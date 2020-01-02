b9baf3749df3568c5858ffe8aa7ccb37230fc60e
Adrian Baran <adrian.baran.30@gmail.com>
2020-01-02 07:46:02 -0600

    feat: add GitHub link to footer and adjust display of error messages

--- src/app/champion-masteries/champion-masteries-error/champion-masteries-error.component.ts
+++ src/app/champion-masteries/champion-masteries-error/champion-masteries-error.component.ts
@@ -27,7 +27,7 @@ export class ChampionMasteriesErrorComponent implements OnInit {
           this.errorMessageLine1 = `Oops! Looks like summoner ${summonerName} has not been found...`;
           this.errorMessageLine2 = 'Please check the name and region, and try again.';
         } else {
-          this.errorMessageLine1 = `Oops! Looks like there has been a server error... `;
+          this.errorMessageLine1 = `Uh oh... Looks like something went wrong on our end... `;
           this.errorMessageLine2 = 'Please try again later.';
         }
       }
